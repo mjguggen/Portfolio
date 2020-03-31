0c14652d1e558ab046d23e464edeb2d439b72483
mjguggen <mjguggen@gmail.com>
2020-03-31 15:04:33 -0500

    updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,9 +5,11 @@ const Footer = props => (
     <div id="footer">
         <div className="container">
             <div className="footer-links">
-                <a href="https://github.com/mjguggen" target="_blank" className="footer-link"> Github </a>
-                <a href="https://www.linkedin.com/in/mike-guggenbuehl/" target="_blank" className="footer-link"> LinkedIn </a>
-                <a className="footer-link" href="mailto:mjguggen@gmail.com?Subject=Hey%20Mike!"> Email </a>
+                <a className="footer-link" href=""> <img className="footer-img" src="https://cdn1.iconfinder.com/data/icons/business-703/80/resume__CV__job__recruitment__file-512.png" download/> </a>
+                <a href="https://github.com/mjguggen" target="_blank" className="footer-link"> <img className="footer-img" src="https://image.flaticon.com/icons/svg/25/25231.svg"/> </a>
+                <a href="https://www.linkedin.com/in/mike-guggenbuehl/" target="_blank" className="footer-link"> <img className="footer-img" src="https://cdn3.iconfinder.com/data/icons/social-media-black-white-2/512/BW_Linkedin_glyph_svg-512.png"/> </a>
+                <a className="footer-link" href="mailto:mjguggen@gmail.com?Subject=Hey%20Mike!"> <img className="footer-img" src="https://cdn.iconscout.com/icon/free/png-256/gmail-32-761667.png"/> </a>
+
             </div>
 
             <div className="copyright"> © Mike Guggenbuehl 2020</div>
@@ -16,3 +18,5 @@ const Footer = props => (
 )
 
 export default Footer
+
+
